1e916f15f8ba19e74e24e25c2c62fab0a0a5b2cb
Charith Ellawala <charith@lucidelectricdreams.com>
2023-10-02 14:19:30 +0100

    WIP lazy nvim
    Signed-off-by: Charith Ellawala <charith@lucidelectricdreams.com>

--- config/nvim/lua/cfg/bootstrap.lua
+++ config/nvim/lua/cfg/bootstrap.lua
@@ -1,25 +1,187 @@
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  vim.notify("Installing packer...")
-  packer_bootstrap = fn.system({
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
   })
-  vim.cmd([[packadd packer.nvim]])
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost bootstrap.lua source <afile> | PackerCompile
-  augroup end
-]])
+require("lazy").setup({
+    "majutsushi/tagbar",
+    "tpope/vim-fugitive",
+    "tpope/vim-surround",
+    "tpope/vim-unimpaired",
+    "preservim/nerdtree",
+    "Xuyuanp/nerdtree-git-plugin",
+    "kana/vim-textobj-user",
+    "reedes/vim-textobj-quote",
+    "junegunn/vim-peekaboo",
+    "bufbuild/vim-buf",
+    "ray-x/lsp_signature.nvim",
+    "lspcontainers/lspcontainers.nvim",
+    "p00f/nvim-ts-rainbow",
+    "NoahTheDuke/vim-just",
+    {
+        "folke/tokyonight.nvim",
+        lazy=false,
+        priority=1000,
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        version = "2.*",
+        build = "make install_jsregexp",
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+        },
+    },
+    {
+        "nvim-tree/nvim-web-devicons",
+        lazy=true,
+    },
+    {
+        "fatih/vim-go",
+        build=":GoUpdateBinaries",
+    },
+    {
+        "terrastruct/d2-vim",
+        lazy=true,
+        ft={"d2"},
+    },
+    {
+        "hashivim/vim-terraform",
+        lazy=true,
+        ft={"terraform"}
+    },
+    {
+        "nvim-lua/plenary.nvim",
+        lazy=true,
+    },
+    {
+        "rust-lang/rust.vim",
+        lazy=true,
+        ft={"rust"},
+    },
+    {
+        "simrat39/rust-tools.nvim",
+        lazy=true,
+        ft={"rust"},
+    },
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("fzf-lua").setup({
+                grep = {
+                    rg_opts="--column --line-number --no-heading --color=always --smart-case --hidden -g \"!.git/*\""
+                },
+            })
+            vim.cmd[[
+                command! -bang -nargs=* Rg lua require('fzf-lua').grep({search=<q-args>})
+            ]]
+        end,
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        version = false,
+        dependencies = {
+          "hrsh7th/cmp-nvim-lsp",
+          "hrsh7th/cmp-buffer",
+          "hrsh7th/cmp-path",
+          "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+            require("plugins.cmp")
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("plugins.lspconfig")
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = false,
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            init = function()
+                require("plugins.tstextobjects")
+            end,
+        },
+        config = function()
+            require("plugins.treesitter")
+        end,
+    },
+    {
+        "mfussenegger/nvim-lint",
+        config = function()
+            require('lint').linters_by_ft = {
+                go = {'golangcilint',},
+                sh = {'shellcheck',},
+                markdown = {'vale',},
+                asciidoc = {'vale',},
+                text = {'vale',},
+                yaml = {'yamllint',},
+            }
+        end,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup()
+        end,
+    },
+    {
+        "folke/trouble.nvim",
+        config = function()
+            require("plugins.trouble")
+        end,
+        dependencies = {"nvim-tree/nvim-web-devicons"},
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        config = function()
+            require("plugins.telescope")
+        end,
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        config = function()
+            require("plugins.lualine")
+        end,
+    },
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+    },
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("plugins.autopairs")
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "hrsh7th/nvim-cmp",
+        }
+    },
+    {
+        "ellisonleao/glow.nvim",
+        config = function()
+            require('glow').setup()
+        end,
+    },
+})
 
+
+
+--[=====[
 return require("packer").startup(function(use)
     use({ 'nvim-tree/nvim-web-devicons' })
     use({ 'nvim-lua/plenary.nvim' })
@@ -174,3 +336,4 @@ return require("packer").startup(function(use)
     require("packer").sync()
   end
 end)
+--]=====]
--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -31,7 +31,8 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-local luasnip = require("luasnip")
+vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
 local cmp = require('cmp')
 
 cmp.setup({
@@ -57,29 +58,49 @@ cmp.setup({
       { name = "buffer" },
       { name = "path" },
   }),
-  mapping = {
+  snippet = {
+    expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  experimental = {
+    ghost_text = {
+      hl_group = "CmpGhostText",
+    },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<S-CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+      --elseif luasnip.expand_or_jumpable() then
+      --  luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
       else
         fallback()
       end
     end, { "i", "s" }),
-
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+      --elseif luasnip.jumpable(-1) then
+      --  luasnip.jump(-1)
       else
         fallback()
       end
     end, { "i", "s" }),
-  },
+  }),
 })
 
 cmp.setup.cmdline('/', {
