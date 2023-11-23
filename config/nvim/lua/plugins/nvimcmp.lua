90b0fbbec29d1d90d042e7297cef56617d81aaba
Charith Ellawala <charith@lucidelectricdreams.com>
2023-11-23 15:00:50 +0000

    Switch off select
    Signed-off-by: Charith Ellawala <charith@lucidelectricdreams.com>

---
+++ config/nvim/lua/plugins/nvimcmp.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = {
+      -- completion = { completeopt = "menu,menuone,noinsert,noselect" },
+      preselect = require("cmp").PreselectMode.None,
+    },
+  },
+}
