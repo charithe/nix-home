14eea60e417865120d73782aaf045dd61b03870f
Charith Ellawala <charith@lucidelectricdreams.com>
2024-05-21 10:33:30 +0100

    Fix trouble icons
    Signed-off-by: Charith Ellawala <charith@lucidelectricdreams.com>

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -1,15 +1,23 @@
 return {
-  --[[
   {
     "folke/trouble.nvim",
+    branch = "dev",
     opts = {
-      mode = "document_diagnostics",
       auto_open = true,
       auto_close = true,
-      auto_preview = false,
-      icons = true,
+      auto_preview = true,
     },
+    config = function()
+      vim.diagnostic.config({
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+            [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+            [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+            [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+          },
+        },
+      })
+    end,
   },
-    ]]
-  --
 }
