4aa5ddc6c2f9fa7940ecaa2ca0591037b97d74c0
Charith Ellawala <charith@lucidelectricdreams.com>
2024-05-23 09:17:19 +0100

    Update flake
    Signed-off-by: Charith Ellawala <charith@lucidelectricdreams.com>

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     -- import/override with your plugins
