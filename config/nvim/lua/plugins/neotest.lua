f8316004fdcb4891c90129b871d739fe4938e3f4
Charith Ellawala <charith@lucidelectricdreams.com>
2024-11-13 17:21:57 +0000

    Configure neotest
    Signed-off-by: Charith Ellawala <charith@lucidelectricdreams.com>

--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -1,10 +1,17 @@
 return {
   {
     "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      { "fredrikaverpil/neotest-golang", version = "*" },
+    },
     opts = {
       adapters = {
-        ["neotest-go"] = {
-          args = { "-tags=tests,integration" },
+        ["neotest-golang"] = {
+          go_test_args = { "-tags=tests,integration" },
         },
       },
     },
