2ed977eb85835c7e2c1cf7e0049179bd34cb966d
Charith Ellawala <charith@lucidelectricdreams.com>
2025-02-23 15:39:21 +0000

    Neovim pkl
    Signed-off-by: Charith Ellawala <charith@lucidelectricdreams.com>

---
+++ config/nvim/lua/plugins/pkl.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "apple/pkl-neovim",
+    lazy = true,
+    ft = "pkl",
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter",
+      },
+    },
+    build = function()
+      require("pkl-neovim.internal").init()
+      vim.cmd("TSInstall! pkl")
+    end,
+  },
+}
