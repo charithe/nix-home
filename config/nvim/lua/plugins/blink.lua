33cb4eb08a4b3e494cc918b6d2a8ece659b5cf12
Charith Ellawala <charith@lucidelectricdreams.com>
2026-02-27 15:30:42 +0000

    Update flake
    Signed-off-by: Charith Ellawala <charith@lucidelectricdreams.com>

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -2,5 +2,28 @@ return {
   {
     "saghen/blink.cmp",
     version = "1.*",
+    dependencies = {
+      "moyiz/blink-emoji.nvim",
+    },
+    opts = {
+      sources = {
+        default = {
+          "emoji",
+        },
+        providers = {
+          emoji = {
+            module = "blink-emoji",
+            name = "Emoji",
+          },
+          path = {
+            opts = {
+              get_cwd = function(_)
+                return vim.fn.getcwd()
+              end,
+            },
+          },
+        },
+      },
+    },
   },
 }
