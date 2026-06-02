77acf44b95cc01ef0b18d856cfe29a731c58e40f
Charith Ellawala <charith@lucidelectricdreams.com>
2026-06-02 07:26:10 +0100

    Update flake
    Signed-off-by: Charith Ellawala <charith@lucidelectricdreams.com>

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -1,6 +1,13 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
     opts = {
       close_if_last_window = true,
     },
